963571cad03ec714a59171dce1ce83658a43bc61
Damien Lebrun <dinoboff@hotmail.com>
2016-06-05 15:31:27 +0100

    Show how to overwrite constants

--- index.js
+++ index.js
@@ -7,6 +7,12 @@
 import angular from 'angular';
 import exampleApp from 'example-app';
 
+const module = angular.module('exampleApp.bootstrap', [exampleApp.name]);
+
+// Overwrite constant here; e.g.:
+//
+// module.constant('firebaseApp', firebase.initializeApp([...]));
+
 angular.element(document).ready(function() {
-  angular.bootstrap(document, [exampleApp.name], {strictDi: true});
+  angular.bootstrap(document, [module.name], {strictDi: true});
 });
