8c5351e497b59009e84b947918060d8295956d04
Damien Lebrun <dinoboff@hotmail.com>
2016-06-05 15:31:49 +0100

    Document service.js

--- src/services.js
+++ src/services.js
@@ -8,18 +8,36 @@
 export class Lists {
 
   constructor() {
-    this.lists = {};
+    this._lists = {};
   }
 
+  /**
+   * Return the lists names.
+   *
+   * The list is sorted.
+   *
+   * @return {array}
+   */
   getLists() {
-    return Object.keys(this.lists).sort();
+    return Object.keys(this._lists).sort();
   }
 
+  /**
+   * Return a sorted list.
+   *
+   * If the list that name does not exist it creates it.
+   *
+   * The list will include `add` and `remove` methods. Items added directly
+   * to list might not get saved and won't keep the list in order.
+   *
+   * @param  {string} name
+   * @return {array}
+   */
   getListById(name) {
-    let list = this.lists[name];
+    let list = this._lists[name];
 
     if (!list) {
-      list = this.lists[name] = [];
+      list = this._lists[name] = [];
     }
 
     list.add = item => {
@@ -42,20 +60,26 @@ export class Lists {
     return list;
   }
 
+  /**
+   * Create a new list.
+   *
+   * @param {name} name
+   */
   create(name) {
-    if (this.lists[name]) {
+    if (this._lists[name]) {
       return;
     }
 
-    this.lists[name] = [];
+    this._lists[name] = [];
 
-    return this.lists[name];
+    return this._lists[name];
   }
 
   remove(name) {
-    delete this.lists[name];
+    delete this._lists[name];
   }
 
 }
 
+// See https://docs.angularjs.org/guide/di#-inject-property-annotation
 Lists.$inject = [];
