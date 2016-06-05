f9281b682bfd5e3be886075fd7f434a36c4678e9
Damien Lebrun <dinoboff@hotmail.com>
2016-06-05 15:31:09 +0100

    merge config.js and main script.

--- src/example-app.js
+++ src/example-app.js
@@ -4,8 +4,6 @@
 import angular from 'angular';
 import 'angular-route';
 
-import * as config from './config.js';
-
 import * as services from './services.js';
 
 import {component as exampleApp} from './components/example-app/example-app.js';
@@ -20,6 +18,32 @@ module.component('exampleApp', exampleApp);
 module.component('eaShopping', eaShopping);
 module.component('eaShoppingLists', eaShoppingLists);
 
-module.config(config.route);
+/**
+ * Configure ngRoute.
+ *
+ * See:
+ *
+ * - $routeProvider API:
+ *   https://docs.angularjs.org/api/ngRoute/provider/$routeProvider
+ *
+ * - component as route template:
+ *   https://docs.angularjs.org/guide/component#components-as-route-templates
+ *
+ * @param  {Object} $routeProvider
+ */
+module.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      template: '<ea-shopping-lists></ea-shopping-lists>'
+    })
+    .when('/lists/:listId', {
+      template: '<ea-shopping list-id="$resolve.params.listId"></ea-shopping">',
+      // Adds `params` to the template $resolve scope object.
+      resolve: {
+        params: ['$route', $route => $route.current.params]
+      }
+    })
+    .otherwise('/');
+}]);
 
 export default module;
